6c3a6c861c2fae8b29ffc7b13263536f82f4a806
atom058 <anton.dahlstrom508@gmail.com>
2015-07-09 17:45:36 +0200

    Add secure password to People

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709152752) do
+ActiveRecord::Schema.define(version: 20150709154318) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -23,8 +23,9 @@ ActiveRecord::Schema.define(version: 20150709152752) do
   create_table "people", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   add_index "people", ["email"], name: "index_people_on_email", unique: true
